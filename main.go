32f7388a2fab8cc797c33796f5901867d6e5fad9
rucciva <rucciva@gmail.com>
2025-08-16 16:15:47 +0700

    add environment

--- main.go
+++ main.go
@@ -24,11 +24,14 @@ var (
 	dbHost = os.Getenv("DATABASE_HOST")
 	dbPort = os.Getenv("DATABASE_PORT")
 	dbName = os.Getenv("DATABASE_NAME")
+	appEnv = os.Getenv("APP_ENV")
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		formTpl.Execute(w, nil)
+		formTpl.Execute(w, map[string]string{
+			"Env": appEnv,
+		})
 	})
 
 	http.HandleFunc("/change", func(w http.ResponseWriter, r *http.Request) {
@@ -44,18 +47,24 @@ func main() {
 		connStr := fmt.Sprintf("host=%s port=%s  dbname=%s  user=%s password=%s sslmode=disable", dbHost, dbPort, dbName, username, password)
 		db, err := sql.Open("postgres", connStr)
 		if err != nil {
-			messageTpl.Execute(w, map[string]string{"Error": "DB connection failed: " + err.Error()})
+			messageTpl.Execute(w, map[string]string{
+				"Error": "DB connection failed: " + err.Error(),
+			})
 			return
 		}
 		defer db.Close()
 
 		_, err = db.Exec(fmt.Sprintf(`ALTER USER "%s" WITH PASSWORD '%s'`, username, newPassword))
 		if err != nil {
-			messageTpl.Execute(w, map[string]string{"Error": "Password change failed: " + err.Error()})
+			messageTpl.Execute(w, map[string]string{
+				"Error": "Password change failed: " + err.Error(),
+			})
 			return
 		}
 
-		messageTpl.Execute(w, map[string]string{"Message": "Password changed successfully!"})
+		messageTpl.Execute(w, map[string]string{
+			"Message": "Password changed successfully!",
+		})
 	})
 
 	log.Println("Server started at :8080")
